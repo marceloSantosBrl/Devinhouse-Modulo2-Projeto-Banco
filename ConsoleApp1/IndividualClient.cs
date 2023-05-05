52d9c9d1e398070af64674b646efae1ec839cb91
marcelo <marcelegald2@outlook.com>
2023-05-05 20:25:46 -0300

    [M2S05] Ex 2 - Criar a classe PessoaFisica

--- ConsoleApp1/IndividualClient.cs
+++ ConsoleApp1/IndividualClient.cs
@@ -5,6 +5,7 @@ public sealed class IndividualClient: Client
     public override int Number { get; set; }
     public override decimal Balance { get; set; }
     public override string Address { get; set; }
+    public string Cpf { get; set; }
     public DateTime BirthDay { get; set; }
     public override void GetSummary()
     {
@@ -14,9 +15,20 @@ public sealed class IndividualClient: Client
         Console.WriteLine(BirthDay);
     }
 
-    public IndividualClient(string address, DateTime birthDay)
+    public IndividualClient(string address, DateTime birthDay, string cpf)
     {
         Address = address;
         BirthDay = birthDay;
+        Cpf = cpf;
+    }
+
+    public static bool IsLegalAge(DateTime birthday)
+    {
+        if (birthday > DateTime.Now)
+        {
+            throw new ArgumentOutOfRangeException(nameof(birthday), $"Data de nascimento{birthday} é maior " +
+                                                                    $"que a data de hoje");
+        }
+        return (birthday.AddYears(18) <= DateTime.Now);
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ConsoleApp1;
+
+Console.WriteLine(IndividualClient.IsLegalAge(DateTime.Today.AddYears(1)));
